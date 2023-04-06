5e4dec65c4cc8f213c99fd0a9c20ea43b3d4303e
Joshua JJ <akhidenorjoshua@gmail.com>
2023-04-06 15:28:42 +0100

    Add upload file functionality

--- src/controllers/uploadsController.js
+++ src/controllers/uploadsController.js
@@ -15,29 +15,16 @@ const uploadTaskFile = async (req, res) => {
 
   const { file } = req.files;
   if (!file) throw new BadRequestError('No file uploaded');
-  let taskFiles;
-  file instanceof Array
-    ? (taskFiles = file)
-    : (taskFiles = Object.values(req.files));
-  let fileLinks = [];
-
-  if (taskFiles.length > 3) {
-    throw new BadRequestError('You cannot upload more than three files');
-  }
-  for (const file of taskFiles) {
-    //! Check if file size limit (10MB) is exceeded
-    if (file.size > process.env.MAX_FILE_SIZE)
-      throw new BadRequestError('File should be less than 10MB');
-    const { tempFilePath } = file;
-    const result = await cloudinary.uploader.upload(tempFilePath, {
-      use_filename: true,
-      folder: 'Todo App',
-    });
-    fileLinks.push(result.secure_url);
-    unlinkSync(tempFilePath);
-    // console.log(result.secure_url);
-  }
-  res.status(StatusCodes.OK).json({ fileLinks });
+  //! Check if file size limit (10MB) is exceeded
+  if (file.size > process.env.MAX_FILE_SIZE)
+    throw new BadRequestError('File should be less than 10MB');
+  const { tempFilePath } = file;
+  const result = await cloudinary.uploader.upload(tempFilePath, {
+    use_filename: true,
+    folder: 'Todo App',
+  });
+  unlinkSync(tempFilePath);
+  res.status(StatusCodes.OK).json({ file: result.secure_url });
 };
 
 module.exports = { uploadTaskFile };
